e8b86bc5b308b26faf319fec217fa55a3156fc20
lagalapa <lagalapa@gmail.com>
2018-08-17 12:19:29 +0500

    Create BinarySearchRecursive.cs

---
+++ BinarySearchRecursive.cs
@@ -0,0 +1,11 @@
+int BS(int[] array, int number, int left, int right)
+{
+    if (left < right)
+    {
+        int middle = (left + right) / 2;
+        if (number > array[middle])
+            return BS(array, number, left + 1, right);
+        return BS(array, number, left, middle);
+    }
+    return (array[left] == number) ? left : -1;
+}
