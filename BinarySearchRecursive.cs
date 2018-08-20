20de59bafd224622574c04f0eb0532e65aba47cf
lagalapa <lagalapa@gmail.com>
2018-08-20 18:11:45 +0500

    Update BinarySearchRecursive.cs

--- BinarySearchRecursive.cs
+++ BinarySearchRecursive.cs
@@ -4,7 +4,7 @@ int BS(int[] array, int number, int left, int right)
     {
         int middle = (left + right) / 2;
         if (number > array[middle])
-            return BS(array, number, left + 1, right);
+            return BS(array, number, middle + 1, right);
         return BS(array, number, left, middle);
     }
     return (array[left] == number) ? left : -1;
